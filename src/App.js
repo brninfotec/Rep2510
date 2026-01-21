6a959328ea0fcdeabe15593c578fb9561c9b16ff
unknown <brninfotec>
2026-01-21 12:37:25 +0530

    added initial files and created fn,ln

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <h1>Signup</h1>
+        <div>
+          <label>First Name</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input></input>
+        </div>
+      </form>
+          </div>
+  );
+}
+
+export default App;
